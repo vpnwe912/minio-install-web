1ba1d746a5cb6928a2b63bfe0d85bb27f7c1b9c5
MuPoBoU <svobodadl@ukr.net>
2025-06-19 19:22:28 +0300

    update instal

--- install.sh
+++ install.sh
@@ -225,10 +225,9 @@ EOF2
     sudo systemctl daemon-reload; sudo systemctl start minio; sudo systemctl enable --now minio
 '
 run_step 85 "Installing MinIO Client" bash -c "if [ \"$MC_INSTALL_TYPE\" = \"1\" ]; then wget -O /tmp/mc https://dl.min.io/client/mc/release/linux-amd64/mc; sudo cp /tmp/mc /usr/local/bin/mc; else sudo cp /var/www/web-minio/downloads/minio-client-debian/mc /usr/local/bin/mc; fi; sudo chmod +x /usr/local/bin/mc"
-run_step 87 "Configuring mc alias" mc alias set $MINIO_ALIAS $MINIO_HOST $MINIO_KEY $MINIO_SECRET
+run_step 87 "Configuring mc alias" mc alias set "$MINIO_ALIAS" "$MINIO_HOST" "$MINIO_KEY" "$MINIO_SECRET"
 run_step 90 "Configuring nginx" bash -c '
 
-run_step 90 "Configuring nginx" bash -c "
 PROJECT_PATH=\"/var/www/web-minio\"
 NGINX_CONF=\"/etc/nginx/sites-available/$DOMAIN\";
 sudo tee \$NGINX_CONF > /dev/null <<EOF2
