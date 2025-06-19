70f2b82ccb1f6aeb55c00a741585c90e84fc9801
MuPoBoU <svobodadl@ukr.net>
2025-06-19 20:14:33 +0300

    update install

--- install.sh
+++ install.sh
@@ -248,7 +248,7 @@ server {
     location ~ \.php$ {
         include snippets/fastcgi-php.conf;
         fastcgi_pass unix:/run/php/php8.3-fpm.sock;
-        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_param SCRIPT_FILENAME $document_root $fastcgi_script_name;
         include fastcgi_params;
     }
     location ~* \.(js|css|png|jpg|jpeg|gif|ico|svg)$ {
